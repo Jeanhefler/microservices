c679407661ca136abd99c9be8813195f8d2f4bc7
Jeanhefler <jeanhefler@outlook.com>
2026-04-09 19:45:27 -0300

    don't repeat yourself

--- greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
@@ -7,16 +7,9 @@ import br.edu.atitus.greeting_service.configs.GreetingConfig;
 
 import java.util.Map;
 
-
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-
-    /*@Value("${greeting-service.greeting}")
-    private String greeting;
-
-    @Value("${greeting-service.default-name}")
-    private String defaultGreeting;*/
 
     private final GreetingConfig config;
     
@@ -24,29 +17,25 @@ public class GreetingController {
         this.config = config;
     }
 
-    @GetMapping({"", "/"})
-    public String getGreeting(@RequestParam(required = false) String name){
+    private String validName(String name){
         if(name == null || name.isEmpty()){
             name = config.getDefaultName();
         }
         String greetingReturn = String.format("%s %s!!!", config.getGreeting(), name);
         return greetingReturn;
     }
+
+    @GetMapping({"", "/"})
+    public String getGreeting(@RequestParam(required = false) String name){
+        return validName(name);
+    }
     @GetMapping("/{name}")
     public String getGreetingName(@PathVariable(required = false) String name){
-        if(name == null || name.isEmpty()){
-            name = config.getDefaultName();
-        }
-        String greetingReturn = String.format("%s %s!!!", config.getGreeting(), name);
-        return greetingReturn;
+       return validName(name);
     }
     @PostMapping()
     public ResponseEntity postGreeting(@RequestBody Map<String, String> body){
         String name = body.get("name");
-        if(name == null || name.isEmpty()){
-            name = config.getDefaultName();
-        }
-        String greetingReturn = String.format("%s %s!!!", config.getGreeting(), name);
-        return ResponseEntity.status(201).body(greetingReturn);
+        return ResponseEntity.status(201).body(validName(name));
     }
 }
